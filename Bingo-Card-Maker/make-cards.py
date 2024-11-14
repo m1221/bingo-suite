e4ebee1e46038cedf4ad0a98734c184179f34b7e
Mario <marioportocarrero7@outlook.com>
2024-11-14 12:16:32 +0800

    add 'number' mode, eg `make-cards.py -n 1-50`
    number mode uses a user-defined number range instead of images

--- Bingo-Card-Maker/make-cards.py
+++ Bingo-Card-Maker/make-cards.py
@@ -11,6 +11,7 @@ parser.add_argument("-o", "--output", help="directory where png cards are saved 
 parser.add_argument("-a", "--side_length", help="size of a bingo square, default = 60", default=60, type=int)
 parser.add_argument("-p", "--private", help="sets the image icon directory to '../private-source-images/individual-icons'; overrides '--source'.", action=argparse.BooleanOptionalAction)
 parser.add_argument("-w", "--words", help="writes words extracted from the image filenames onto the bottoms of the spaces", action=argparse.BooleanOptionalAction)
+parser.add_argument("-n", "--numbers", help="if set, uses numbers instead of images; enter the range, eg `-n 1-50`", default="unset", type=str)
 
 if (parser.parse_args().private):
     image_dir = Path("../private-source-images/individual-icons")
@@ -19,6 +20,21 @@ else:
 
 pathnames = [pathname for pathname in image_dir.glob("[!.]*")]
 
+
+# utility fn
+def getNumbersFromRange(number_range):
+    try:
+        a, b = number_range.split('-')
+        a = int(a)
+        b = int(b)
+        if b - a >= 24:
+            return [i for i in range(a, b + 1)]
+        raise(ValueError('The range must span at least 25 numbers.'))
+    except BaseException as e:
+        print(f'\'{number_range}\' is an invalid range. {e}')
+        exit()
+
+# utility fn
 def getUserFriendlyName(img_path):
     # '01_spider-web.png' => 'spider web'
     temp = ' '.join(((img_path.name.split('_')[1]).split('.')[0]).split('-'))
@@ -46,11 +62,13 @@ def makeBingoCard(image_pathnames, id, side_length=parser.parse_args().side_leng
         font = ImageFont.truetype("/usr/share/fonts/truetype/lyx/dsrom10.ttf", int(side_length / 50) * 10)
         header_font = ImageFont.truetype("/usr/share/fonts/truetype/lyx/dsrom10.ttf", int(side_length / 25) * 10)
         footer_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf", 10)
+        number_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", int(side_length / 50) * 22)
         space_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", int(side_length / 50) * 7)
     except IOError:
         font = ImageFont.load_default()
         header_font = ImageFont.load_default()
         footer_font = ImageFont.load_default()
+        number_font = ImageFont.load_default()
         space_font = ImageFont.load_default()
 
     # 3. Draw the Serial Number in the footer
@@ -63,8 +81,12 @@ def makeBingoCard(image_pathnames, id, side_length=parser.parse_args().side_leng
         x = i * side_length + (side_length - draw.textsize(letter, font=header_font)[0]) // 2
         draw.text((x, 2), letter, fill='black', font=header_font)
 
-    # 5. draw the images
-    selected_images = random.sample(image_pathnames, 24)  # 25 total minus 1 free space
+    # 5. draw the images OR  numbers
+    number_range = parser.parse_args().numbers
+    if number_range == "unset":
+        selected_images = random.sample(image_pathnames, 24)  # 25 total minus 1 free space
+    else:
+        selected_numbers = random.sample(getNumbersFromRange(number_range), 24)
 
     for i in range(5):
         for j in range(5):
@@ -77,21 +99,28 @@ def makeBingoCard(image_pathnames, id, side_length=parser.parse_args().side_leng
                 text_size = draw.textsize(text, font=font)
                 draw.text((x + (side_length - text_size[0]) // 2, y + (side_length - text_size[1]) // 2), text, fill='black', font=font)
             else:
-                # Get an image and resize it
-                img_path = selected_images.pop()
-                img = Image.open(img_path).convert('RGBA')  # Ensure image has alpha channel
-                img = img.resize((side_length, side_length), Image.ANTIALIAS)
+                if number_range == "unset":
+                    # Get an image and resize it
+                    img_path = selected_images.pop()
+                    img = Image.open(img_path).convert('RGBA')  # Ensure image has alpha channel
+                    img = img.resize((side_length, side_length), Image.ANTIALIAS)
 
-                # Create a mask for pasting
-                card.paste(img, (x, y), img)  # Use img as the mask to preserve transparency
+                    # Create a mask for pasting
+                    card.paste(img, (x, y), img)  # Use img as the mask to preserve transparency
 
-                # write text to the space
-                if (parser.parse_args().words):
-                    text = getUserFriendlyName(img_path)
-                    text_size = draw.textsize(text, font=space_font)
+                    # write descriptive text to the space
+                    if (parser.parse_args().words):
+                        text = getUserFriendlyName(img_path)
+                        text_size = draw.textsize(text, font=space_font)
+                        x_pos = x + (side_length - text_size[0]) // 2
+                        y_pos = y + (side_length - text_size[1]) * 7 // 8
+                        draw.text((x_pos, y_pos), text, fill='black', font=space_font)
+                else:
+                    number_to_write = str(selected_numbers.pop())
+                    text_size = draw.textsize(number_to_write, font=number_font)
                     x_pos = x + (side_length - text_size[0]) // 2
-                    y_pos = y + (side_length - text_size[1]) * 7 // 8
-                    draw.text((x_pos, y_pos), text, fill='black', font=space_font)
+                    y_pos = y + (side_length - text_size[1]) // 2
+                    draw.text((x_pos, y_pos), number_to_write, fill='black', font=number_font)
 
     # 6. Draw grid lines
     for i in range(6):  # 6 lines for 5 squares
