e65d4eee79782649e6d07c0bc3c2cf20776db56d
Mario <marioportocarrero7@outlook.com>
2024-11-20 12:48:03 +0800

    refactor&fix pool-selection-and-generation code
    fix:
    there was a bug with [] * x instead of ['0'] * x

--- Bingo-Card-Maker/make-cards.py
+++ Bingo-Card-Maker/make-cards.py
@@ -11,7 +11,7 @@ parser.add_argument("-s", "--source", help="the path to the directory that conta
 parser.add_argument("-o", "--output", help="directory where png cards are saved to", default="./bingo-cards", type=str)
 parser.add_argument("-a", "--side_length", help="size of a bingo square, default = 60", default=60, type=int)
 parser.add_argument("-w", "--words", help="writes words extracted from the image filenames onto the bottoms of the spaces", action=argparse.BooleanOptionalAction)
-parser.add_argument("-n", "--number_range", help="if set, uses numbers instead of images; enter the range, eg `-n 1-50`", default="unset", type=str)
+parser.add_argument("-n", "--number_range", help="if set, uses numbers instead of images; enter the range, eg `-n 1-50`", type=str)
 
 def __loadFont__(dir, font_size):
     try:
@@ -26,7 +26,7 @@ def __getNumberListFromUserInput__(number_range):
         a = int(a)
         b = int(b)
         if b - a >= 24:
-            return [i for i in range(a, b + 1)]
+            return [str(i) for i in range(a, b + 1)]
         raise(ValueError('The range must span at least 25 numbers.'))
     except BaseException as e:
         print(f'\'{number_range}\' is an invalid range. {e}')
@@ -74,75 +74,66 @@ def makeBingoCard(id, side_length=parser.parse_args().side_length, line_thicknes
         x = i * side_length + (side_length - draw.textsize(letter, font=header_font)[0]) // 2
         draw.text((x, 2), letter, fill='black', font=header_font)
 
-    # 5. draw the images OR numbers
+    # 5. make selection pools
     number_range = parser.parse_args().number_range
 
     # 5.A check if should use one pool
     if parser.parse_args().unique:
-        if number_range == "unset":
-            selected_images = random.sample(image_pathnames, 24)  # 25 total minus 1 free space
+        if number_range:
+            selection_pool = random.sample(__getNumberListFromUserInput__(number_range), 25)
         else:
-            selected_numbers = random.sample(__getNumberListFromUserInput__(number_range), 24)
+            selection_pool = random.sample(image_pathnames, 25)
     else: #5.B use multiple pools
-        # TODO: make code for multiple pools more intuitive and easier to read
-        bingo = {}
-        if number_range == "unset":
-            for i in range(5):
-                bingo[i] = random.sample(image_pathnames, 5)
-
-            selected_images = [] * 24
-            for k in range(25):
-                if k == 12:
-                    continue
-                source_index = k % 5
-                selected_images.insert(k, bingo[source_index].pop())
-        else:
+        pools = {}
+        if number_range:
             numbers = __getNumberListFromUserInput__(number_range)
-            for i in range(5):
-                bingo[i] = random.sample(numbers, 5)
 
-            selected_numbers = [0] * 24
-            for k in range(25):
-                if k == 12:
-                    continue
-                source_index = k % 5
-                selected_numbers.insert(k, bingo[source_index].pop())
+        for i in range(5):
+            pools[i] = random.sample(numbers, 5) if number_range else random.sample(image_pathnames, 5)
+     
+        selection_pool = ['0'] * 25
+        for k in range(25):
+            source_index = k % 5
+            selection_pool[k] = pools[source_index].pop()
 
+    # 6. draw the images OR numbers
     for i in range(5):
         for j in range(5):
             x = j * side_length
             y = i * side_length + header_spacing  # Adjust for header space
 
             if i == 2 and j == 2:  # Center space
+                selection_pool.pop()
                 draw.rectangle([x, y, x + side_length, y + side_length], fill=(255, 255, 255, 255))
                 text = "FREE"
                 text_size = draw.textsize(text, font=font)
                 draw.text((x + (side_length - text_size[0]) // 2, y + (side_length - text_size[1]) // 2), text, fill='black', font=font)
+                continue
+
+            if number_range:
+                number_to_write = selection_pool.pop()
+                text_size = draw.textsize(number_to_write, font=number_font)
+                x_pos = x + (side_length - text_size[0]) // 2
+                y_pos = y + (side_length - text_size[1]) // 2
+                draw.text((x_pos, y_pos), number_to_write, fill='black', font=number_font)
             else:
-                if number_range == "unset":
-                    # Get an image and resize it
-                    img_path = selected_images.pop()
-                    img = Image.open(img_path).convert('RGBA')  # Ensure image has alpha channel
-                    img = img.resize((side_length, side_length), Image.ANTIALIAS)
+                # Get an image and resize it
+                img_path = selection_pool.pop()
+                img = Image.open(img_path).convert('RGBA')  # Ensure image has alpha channel
+                img = img.resize((side_length, side_length), Image.ANTIALIAS)
 
-                    # Create a mask for pasting
-                    card.paste(img, (x, y), img)  # Use img as the mask to preserve transparency
+                # Create a mask for pasting
+                card.paste(img, (x, y), img)  # Use img as the mask to preserve transparency
 
-                    # write descriptive text to the space
-                    if (parser.parse_args().words):
-                        text = __getDisplayName__(img_path)
-                        text_size = draw.textsize(text, font=space_font)
-                        x_pos = x + (side_length - text_size[0]) // 2
-                        y_pos = y + (side_length - text_size[1]) * 7 // 8
-                        draw.text((x_pos, y_pos), text, fill='black', font=space_font)
-                else:
-                    number_to_write = str(selected_numbers.pop())
-                    text_size = draw.textsize(number_to_write, font=number_font)
+                # write descriptive text to the space
+                if (parser.parse_args().words):
+                    text = __getDisplayName__(img_path)
+                    text_size = draw.textsize(text, font=space_font)
                     x_pos = x + (side_length - text_size[0]) // 2
-                    y_pos = y + (side_length - text_size[1]) // 2
-                    draw.text((x_pos, y_pos), number_to_write, fill='black', font=number_font)
+                    y_pos = y + (side_length - text_size[1]) * 7 // 8
+                    draw.text((x_pos, y_pos), text, fill='black', font=space_font)
 
-    # 6. Draw grid lines
+    # 7. Draw grid lines
     for i in range(6):  # 6 lines for 5 squares
         # Vertical lines
         x = i * side_length
@@ -152,10 +143,10 @@ def makeBingoCard(id, side_length=parser.parse_args().side_length, line_thicknes
         y = i * side_length + header_spacing
         draw.line([(0, y), (card_width, y)], fill='black', width=line_thickness)
 
-    # 7. Save the bingo card to a file
+    # 8. Save the bingo card to a file
     card.save(f'{parser.parse_args().output}/{serial_number}_bingo-card.png', 'PNG')
 
-if parser.parse_args().number_range == 'unset':
+if parser.parse_args().number_range == None:
     image_dir = Path(parser.parse_args().source)
     image_pathnames = [pathname for pathname in image_dir.glob("[!.]*")]
 
