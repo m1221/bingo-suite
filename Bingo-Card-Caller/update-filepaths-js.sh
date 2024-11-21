27ff97a7fe5137c5e5ca63349ce358292152fa9f
Mario <marioportocarrero7@outlook.com>
2024-11-21 09:29:53 +0800

    refactor script that adds filepaths for js usage
    user now has more freedom to determine what directory
    to get images from; also, add instructions for usage

--- Bingo-Card-Caller/update-filepaths-js.sh
+++ Bingo-Card-Caller/update-filepaths-js.sh
@@ -1,14 +1,26 @@
 #! /bin/bash
 # 1. get image file names
 # 2. write them to a js file
-
+#
+# NOTE: this script MUST be run from PROJECT ROOT! 
+# Also, the image directory must be a child of the project root director.
+# EXAMPLES:
+# ./Bingo-Card-Caller/update-filepaths-js.sh
+# ./Bingo-Card-Caller/update-filepaths-js.sh ./source-images/individual-icons
 
 if [ -z "$1" ]; then
-    IMAGE_FOLDER="../source-images/individual-icons"
-elif [ "$1" == "p" ]; then
-    IMAGE_FOLDER="../private-source-images/individual-icons"
+    IMAGE_FOLDER="./source-images/individual-icons"
+elif [ -d "$1" ]; then
+    child=$1
+    result=$(find "." -type d -wholename "$child")
+    if [[ -n $result ]]; then
+        IMAGE_FOLDER="$child"
+    else
+        echo "Please place the image directory inside the project root directory 'BingoSuite'."
+        exit 1
+    fi
 else
-    echo "'$1' is not a valid input. Please do not enter an argument OR enter 'p' to get images from '../private-source-images/individual-icons' instead of '../source-images/individual-icons'."
+    echo "The path \"$1\" does not point to a valid directory."
     exit 1
 fi
 
@@ -16,7 +28,7 @@ pathnames=""
 
 for filename in $(ls $IMAGE_FOLDER)
     do
-        pathname="'../$IMAGE_FOLDER/$filename'"
+        pathname="'../.$IMAGE_FOLDER/$filename'"
         if [ "$pathnames" == "" ]; then
             pathnames="\n    $pathname"
         else
@@ -24,4 +36,4 @@ for filename in $(ls $IMAGE_FOLDER)
         fi
     done
 
-printf "let filepaths = [$pathnames\n];" > "./code/filepaths.js"
+printf "let filepaths = [$pathnames\n];\n" > "./Bingo-Card-Caller/code/filepaths.js"
