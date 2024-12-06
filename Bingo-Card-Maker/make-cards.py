1b3dab69f417e10d8ece5967c6419b7358804ab7
Mario <m.portocarrero.jr@gmail.com>
2024-12-06 14:15:55 +0800

    add shebang to python scripts

--- Bingo-Card-Maker/make-cards.py
+++ Bingo-Card-Maker/make-cards.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 import argparse
 import random
 from PIL import Image, ImageDraw, ImageFont
--- Bingo-Card-Maker/make-pdf.py
+++ Bingo-Card-Maker/make-pdf.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 import argparse
 from pathlib import Path
 from reportlab.lib import pagesizes
